83b9a4f2f08df43127f8d2d33df58841eb8da9a8
Rutusmita prusty <Rutusmita prusty@LAPTOP-K6JBIRD9>
2025-07-02 19:27:02 +0530

    First Commit

---
+++ Automation_Practice/src/test/java/demo/Launching.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launching {
+	
+	@Test
+	public void launch()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.close();
+	}
+}
